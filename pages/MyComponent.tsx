2240bb8ad1e461a78dd559f41c74ee6befc4d6a5
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-05-24 07:59:34 -0400

    Brougth to correct code

--- pages/MyComponent.tsx
+++ pages/MyComponent.tsx
@@ -4,15 +4,10 @@
 */
 
 import Head from 'next/head';
-import { Children } from 'react';
 
 export const MyComponent = ({ text }: MyProps): JSX.Element => {
   return (
     <div className="myClass">
-      <Head>
-        <title>MyComponent add new title!!</title>
-        <link key={2} rel="icon" href="/RR.ico" />
-      </Head>
       <>{text}</>
     </div>
   );

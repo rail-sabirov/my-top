8b65cb9f49f72c5af243e011cbba762c049ee7e1
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-06-05 08:56:05 -0400

    Fixing: Move Google font initialization to the _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import React from 'react';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -8,12 +9,6 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>MyTop - Наш лучший топ</title>
         <link rel="icon" href="favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,14 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ru">
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main></Main>
           <NextScript></NextScript>

dbe10bef785d504995d2906077dbe2effd0bf50c
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-06-19 08:57:45 -0400

    layout with css-grid

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -7,28 +7,26 @@ import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 import { FunctionComponent } from 'react';
 
-
 // Страница имеет header, центральную часть в которой: левый saidebar и основной вывод, footer
-const Layout = ({children }: LayoutProps): JSX.Element => {
-    return (<>
-        <Header />
-        <div>
-            <Sidebar />
-            <div>
-                {children}
-            </div>
-        </div>
-        <Footer />
-    </>);
-}
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
+  );
+};
 
-// HOC функция, которая получает компонент и оборачивает его передавая ему props
+// HOC - оборачиваем основной компонент HTML шаблоном сайта
+// функция, получает компонент и оборачивает его передавая ему props
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
-    return function withLayoutComponent(props: T): JSX.Element {
-        return (
-            <Layout>
-                <Component {...props} />
-            </Layout>  
-        )
-    }
-}
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
+};

a5ce66ea77cddb1c3ec8ac0385e1c098dfe034c8
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-06-19 16:08:58 -0400

    footer with grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,8 +1,15 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
 import cn from 'classnames';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}> Footer </div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Top © 2020 - {format(new Date(), 'yyyy')} All rights reserved</div>
+      <a href="#" target='blank'>Terms of use</a>
+      <a href="" target='blank'>Privacy Policy</a>
+    </footer>
+  );
 };
